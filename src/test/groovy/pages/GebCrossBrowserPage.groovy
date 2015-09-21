05cd1322c5a9d6b628c6c026c17f122701e5e76c
Marcin Erdmann <erdi84@gmail.com>
2015-09-21 18:26:29 +0100

    Reorganise what we're waiting on to make the at checker for GebCrossBrowserPage even more robust.

--- src/test/groovy/pages/GebCrossBrowserPage.groovy
+++ src/test/groovy/pages/GebCrossBrowserPage.groovy
@@ -3,9 +3,10 @@ package pages
 import geb.Page
 
 class GebCrossBrowserPage extends Page {
-	static at = { headings[0].text() == "Cross Browser Automation" }
+	static at = { firstHeadingText == "Cross Browser Automation" }
 	static content = {
-		headings(wait: true) { $('#main h1') }
+		headings { $('#main h1') }
 		firstHeading { headings[0] }
+		firstHeadingText(wait: true) { firstHeading.text() }
 	}
 }
