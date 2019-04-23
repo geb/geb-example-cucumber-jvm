8a15ddd1e9ebe11e7320318f56dc34d1c9288b0d
Marcin Erdmann <erdi84@gmail.com>
2019-04-23 17:52:26 +0100

    Updated the versions of cucumber-jvm, plugins, Selenium, Groovy, Geb, Browser drivers and i added functionality to execute tests as headless for various browsers.
    # Conflicts:
    #	build.gradle

--- src/test/resources/GebConfig.groovy
+++ src/test/resources/GebConfig.groovy
@@ -6,20 +6,52 @@
 
 
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxDriver
+import org.openqa.selenium.firefox.FirefoxOptions
 
 // Use firefox as the default
 // See: http://code.google.com/p/selenium/wiki/FirefoxDriver
-driver = { new FirefoxDriver() }
+driver = { return firefoxDriver(false) }
 
 environments {
 
-	// run as “gradle -Dgeb.env=chrome cucumber”
+	// run as “./gradlew -Dgeb.env=chrome cucumber”
 	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
 	chrome {
-		driver = { new ChromeDriver() }
+		driver = { return chromeDriver(false) }
 	}
 
+	// run as “./gradlew -Dgeb.env=chromeHeadless cucumber”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	chromeHeadless {
+		driver = { return chromeDriver(true) }
+	}
+
+	// run as “./gradlew -Dgeb.env=firefox cucumber”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	firefox {
+		driver = { return firefoxDriver(false) }
+	}
+
+	// run as “./gradlew -Dgeb.env=firefoxHeadless cucumber”
+	// See: http://code.google.com/p/selenium/wiki/ChromeDriver
+	firefoxHeadless {
+		driver = { return firefoxDriver(true) }
+	}
+
+}
+
+def chromeDriver(boolean headless) {
+	ChromeOptions chromeOptions = new ChromeOptions()
+	if (headless) chromeOptions.addArguments("--headless")
+	new ChromeDriver(chromeOptions)
+}
+
+def firefoxDriver(boolean headless) {
+	FirefoxOptions firefoxOptions = new FirefoxOptions()
+	if (headless) firefoxOptions.addArguments("--headless")
+	return new FirefoxDriver(firefoxOptions)
 }
 
 baseUrl = "http://gebish.org"
